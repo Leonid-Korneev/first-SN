7270d7316e22a08d03c66be2cbc963f8dc71b2ef
Your Name <you@example.com>
2020-09-23 12:48:26 +0300

    Added Dialogs.jsx, Dialogs.module.css; Added a mutual style;

--- src/App.js
+++ src/App.js
@@ -4,32 +4,27 @@ import './App.css';
 import Profile from './components/Profile/Profile';
 
 import NavBar from './components/NavBar/NavBar';
-import Header from './components/Header/Header';
-
+import Header from "./components/Header/Header";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 const App = () => {
-  return (
-   
-    <div className="app-wrapper">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ranchers&display=swap" rel="stylesheet"></link>
-      <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet"></link>
-      <Header/>
-      <NavBar/>
-      <Profile />
+    return (
 
+        <div className="app-wrapper">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ranchers&display=swap"/>
+            <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet"/>
+            <Header/>
+            <NavBar/>
+            <div className="app-wrapper__content">
+                <Dialogs/>
+            </div>
 
+            {/*<Profile />*/}
 
-
-
-
-
-    </div>
-  );
+        </div>
+    );
 }
-
-
-
 
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import MyPosts from './MyPosts/MyPosts';
 import s from "./Profile.module.css"
+
 const Profile = () => {
     return (
-        <div className={s.content}>
-        <div> <img className={s.content__img} src="https://cq.ru/storage/uploads/posts/75938/seo-cover-29960.jpg" />
-        </div>
         <div>
-          ava+descr
+            <div>
+                <img className={s.content__img} alt="" src="https://cq.ru/storage/uploads/posts/75938/seo-cover-29960.jpg"/>
+            </div>
+            <div>
+                ava+descr
+            </div>
+            <MyPosts/>
         </div>
-        <MyPosts/>
-      </div>
 
     );
 }
