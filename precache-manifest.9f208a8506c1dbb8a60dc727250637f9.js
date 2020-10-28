974314c719418e8b09f75c351e133f3aad55fe89
Your Name <you@example.com>
2020-10-28 13:00:16 +0300

    Updates

---
+++ precache-manifest.9f208a8506c1dbb8a60dc727250637f9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b8293300ed4e981160259e11c11dd1",
+    "url": "/first-SN/index.html"
+  },
+  {
+    "revision": "eeb7acc93006016b7c8e",
+    "url": "/first-SN/static/css/3.59318d40.chunk.css"
+  },
+  {
+    "revision": "97803ade2be008d5432e",
+    "url": "/first-SN/static/css/main.8824a6c9.chunk.css"
+  },
+  {
+    "revision": "b646ae9a8f72d1a41e0a",
+    "url": "/first-SN/static/js/2.1a1e2563.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/first-SN/static/js/2.1a1e2563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb7acc93006016b7c8e",
+    "url": "/first-SN/static/js/3.f20c0b70.chunk.js"
+  },
+  {
+    "revision": "97803ade2be008d5432e",
+    "url": "/first-SN/static/js/main.f847b1e1.chunk.js"
+  },
+  {
+    "revision": "206a9f40cdf00689401a",
+    "url": "/first-SN/static/js/runtime-main.3bc549dd.js"
+  },
+  {
+    "revision": "302be374ce7b0af4d5b650bf04e81d0b",
+    "url": "/first-SN/static/media/default-avtar.302be374.jpg"
+  },
+  {
+    "revision": "a61312b380967c5bdb237e25112bcda8",
+    "url": "/first-SN/static/media/like.a61312b3.png"
+  },
+  {
+    "revision": "fa7dd23c8d459e949b553886f056d35b",
+    "url": "/first-SN/static/media/preloader.fa7dd23c.svg"
+  }
+]);
